7a29e02693dbe32aabf99d502e6155cb73f5f2bd
dotconfig404 <119617043+dotconfig404@users.noreply.github.com>
2022-12-01 19:20:19 +0100

    part 2 day 1

--- day1/day1.py
+++ day1/day1.py
@@ -10,3 +10,20 @@ with open("input") as f:
         else:
             current_calories = current_calories + int(line)
     print(calories_high)
+
+# part 2
+
+import bisect
+top_3 = [0,0,0]
+
+with open("input") as f:
+    current_calories = 0
+    while (line := f.readline()):
+        if len(line) == 1:
+            if current_calories > top_3[0]:
+                bisect.insort(top_3, current_calories)
+                top_3.pop(0)
+            current_calories = 0
+        else:
+            current_calories = current_calories + int(line)
+    print(top_3[0] + top_3[1] + top_3[2])
